75a77f818127f996e3e9cd859e42ce7b138330d8
mradams276 <kyle.adams276@gmail.com>
2016-10-19 07:42:57 -0600

    Started my project

---
+++ Layout 10-19-16/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Layout 10-19-16
+//
+//  Created by Adams, Kyle on 10/19/16.
+//  Copyright © 2016 Adams, Kyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
